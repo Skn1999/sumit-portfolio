92219c75a3aa01243cdead15d391de189df3744e
skanayyarsk <sknayyar.sk@gmail.com>
2026-06-28 13:51:08 +0200

    updated Accessibility to Availability

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -53,7 +53,7 @@ const HeroSection: React.FC = () => {
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
               </span>
-              <span>IMMEDIATE ACCESSIBILITY</span>
+              <span>IMMEDIATE AVAILABILITY</span>
             </motion.div>
 
             {/* Primary Display Title */}
